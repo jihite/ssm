44d0a2467e1539e3338d6191fd68d83083a9272c
wzh94434 <wzh94434@alibaba-inc.com>
2019-03-21 19:28:45 +0800

    调整代码

--- src/main/java/com/jihite/entity/Appointment.java
+++ src/main/java/com/jihite/entity/Appointment.java
@@ -1,10 +1,15 @@
 package com.jihite.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
 /**
  * 预约图书实体
  */
+@Getter
+@Setter
 public class Appointment {
 
 	private long bookId;// 图书ID
@@ -29,38 +34,6 @@ public class Appointment {
 		this.bookId = bookId;
 		this.studentId = studentId;
 		this.appointTime = appointTime;
-		this.book = book;
-	}
-
-	public long getBookId() {
-		return bookId;
-	}
-
-	public void setBookId(long bookId) {
-		this.bookId = bookId;
-	}
-
-	public long getStudentId() {
-		return studentId;
-	}
-
-	public void setStudentId(long studentId) {
-		this.studentId = studentId;
-	}
-
-	public Date getAppointTime() {
-		return appointTime;
-	}
-
-	public void setAppointTime(Date appointTime) {
-		this.appointTime = appointTime;
-	}
-
-	public Book getBook() {
-		return book;
-	}
-
-	public void setBook(Book book) {
 		this.book = book;
 	}
 
--- src/main/java/com/jihite/entity/Book.java
+++ src/main/java/com/jihite/entity/Book.java
@@ -1,8 +1,13 @@
 package com.jihite.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * 图书实体
  */
+@Getter
+@Setter
 public class Book {
 
 	private long bookId;// 图书ID
@@ -17,30 +22,6 @@ public class Book {
 	public Book(long bookId, String name, int number) {
 		this.bookId = bookId;
 		this.name = name;
-		this.number = number;
-	}
-
-	public long getBookId() {
-		return bookId;
-	}
-
-	public void setBookId(long bookId) {
-		this.bookId = bookId;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getNumber() {
-		return number;
-	}
-
-	public void setNumber(int number) {
 		this.number = number;
 	}
 
